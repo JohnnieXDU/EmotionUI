a838d790a289e6afa86d74680b378ca6d730e61f
Yang Jiao <yjiao.xdu@gmail.com>
2021-12-16 13:48:09 +0800

    Update main_train.py

--- train/main_train.py
+++ train/main_train.py
@@ -1,15 +1,16 @@
-from collect_data import create_data_folder
+from collect_data import create_data_folder, collect_train_data
 from train_model import train
 
 if __name__ == "__main__":
     class_label = ['NA', 'AN', 'DI', 'FE', 'HA', 'SA', 'SU']
 
-    # Step 1. Collect data
+    # Step 1. Automatic create train/val folders
     create_data_folder()
 
-    # classes: natural / angry / disgust / fear / happy / sad / surprise
-    # collect_train_data(exp="surprise", save=True)
+    # Step 2. Data collection
+    # - Collect train/val data by setting 'exp' (expression) with: natural / angry / disgust / fear / happy / sad / surprise
+    collect_train_data(exp="surprise", save=True)
 
-    # Step 2. Train
+    # Step 3. Network training
     train()
 
